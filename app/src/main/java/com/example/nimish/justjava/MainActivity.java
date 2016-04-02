a63d0c4f5eac3d60268a47597077d4f311691385
nimishjain1000 <nimishjain1000@gmail.com>
2016-04-02 23:34:21 +0530

    added mail feature

--- app/src/main/java/com/example/nimish/justjava/MainActivity.java
+++ app/src/main/java/com/example/nimish/justjava/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.nimish.justjava;
 
 
         import android.content.Context;
+        import android.content.Intent;
         import android.os.Bundle;
         import android.support.v7.app.ActionBarActivity;
+        import android.text.Editable;
+        import android.util.Log;
         import android.view.View;
+        import android.widget.CheckBox;
+        import android.widget.EditText;
         import android.widget.TextView;
         import android.widget.Toast;
 
@@ -25,11 +30,33 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String msg="Cost of "+display(quantity)+" is "+displayPrice(20 * quantity)+ "$";
+        CheckBox whippedCreamCheckBox= (CheckBox) findViewById(R.id.notify_me_checkbox);
+       /* if (((CheckBox) view).isChecked()) {
+            Toast.makeText(MainActivity.this,
+                    "whipped cream added:)", Toast.LENGTH_LONG).show();
 
+        }*/
+        boolean hasWhippedcream=whippedCreamCheckBox.isChecked();
+        Log.v("main activity","has whipped cream:"+hasWhippedcream);
+        String msg;
+if(hasWhippedcream)
+{
+    msg=createOrderSummary()+"Cost of "+display(quantity)+" is "+displayPrice(22 * quantity)+ "$";
+
+}
+       else {
+    msg = createOrderSummary()+"Cost of " + display(quantity) + " is " + displayPrice(20 * quantity) + "$";
+}
         display(quantity);
        // displayPrice(2 * quantity);
-        displayMessage(msg);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_EMAIL, "nimishjain1000@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Your coffee summary");
+        intent.putExtra(Intent.EXTRA_TEXT, msg);
+
+        startActivity(Intent.createChooser(intent, "Send Email"));
+       // displayMessage(msg);
     }
     private void displayMessage(String msg)
     {
@@ -64,11 +91,26 @@ public class MainActivity extends ActionBarActivity {
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
+
+    }
+    boolean isChecked=false;
+    public String check()
+    {
+        EditText editText = (EditText) findViewById(R.id.name);
+        String value = editText.getText().toString();
+        return value;
+
     }
     private int displayPrice(int number) {
         //TextView priceTextView = (TextView) findViewById(
           //      R.id.price_text_view);
         //priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
         return number;
+    }
+    private String createOrderSummary()
+    {
+        String a="Hello "+check()+",your order summary is";
+        a+="Quantity "+display(quantity);
+        return a;
     }
 }
